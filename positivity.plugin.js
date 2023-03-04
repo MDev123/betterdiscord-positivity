36711789ff44323fa667acb93adb037481c325d3
MDev123 <86262715+MDev123@users.noreply.github.com>
2023-03-04 12:11:58 -0500

    Create positivity.plugin.js

---
+++ positivity.plugin.js
@@ -0,0 +1,105 @@
+/**
+ * @name Positivity!
+ * @version 0.1.0
+ * @description Enjoy great positivity.
+ * @author kno
+  * @source https://github.com/MDev123/betterdiscord-positivity
+ * @updateUrl https://mdev123.github.io/betterdiscord-positivity/positivity.plugin.js
+  * @changelogImage https://media.discordapp.net/attachments/1080608995083042896/1081622153943855124/Untitled-10.jpg?width=1726&height=970
+ */
+
+const version = Number("0.1.1".replaceAll('.', ''))
+
+const config = {
+    info: {
+        name: "Positivity",
+        authors: [
+            {
+                name: "kno",
+            },
+        ],
+        version: "0.1.1",
+        description: "Great positivity!",
+    },
+    changelog: [
+        {
+            title: "**Updates**",
+            type: "added",
+            items: ["Added more positivity quotes.", "Added auto-updater, so you don't need to!"],
+        },
+        {
+            title: "**Bug Fixes**",
+            type: "fixed",
+            items: ["Fixed a bug where the same quote could be shown twice in a row.", "Fixed "],
+        },
+        {
+            title: "# Twitter",
+            type: "fixed",
+            items: ["Follow us on twitter! https://twitter.com/BPositivityLife"]
+        },
+    ],
+    defaultConfig: [],
+};
+
+module.exports = class Positivity {
+    start() {
+        console.log("[Positivity] Plugin started.");
+
+        // Show changelog
+        const changes = config.changelog
+            .map((change) => `### ${change.title}\n${change.items.map((item) => `- ${item}`).join("\n")}`)
+            .join("\n\n");
+        const changelog = `# Changelog\n\n${changes}`;
+        BdApi.alert("💡 Positivity 💡", changelog);
+
+        // Array of positivity quotes
+        const positivityQuotes = [
+            "You are amazing just the way you are!",
+            "Don't be afraid to dream big and chase your passions!",
+            "Believe in yourself and you can accomplish anything!",
+            "Every day is a new opportunity to make a positive change!",
+            "You are capable of overcoming any obstacle!",
+            "The world needs your unique talents and perspectives!",
+            "You are deserving of love and happiness!",
+            "You are stronger than you think!",
+            "Challenges are opportunities for growth and learning!",
+            "Your potential is limitless!",
+            "You have the power to make a difference in someone's life today!",
+            "Good things are coming your way, so keep your head up!",
+            "You are not alone, and there are people who care about you!",
+            "You are worthy of respect and kindness!",
+            "You are making progress, even if it doesn't always feel like it!",
+            "You are enough, just as you are!",
+            "You are capable of creating your own happiness!",
+            "You have the power to inspire others!",
+            "Believe in your dreams, and they will come true!",
+            "You are capable of great things, so don't give up!",
+        ];
+
+        // Get a random positivity quote from positivityQuotes
+        const randomQuote =
+            positivityQuotes[Math.floor(Math.random() * positivityQuotes.length)];
+
+        BdApi.alert("💡 Positivity 💡", randomQuote);
+    }
+
+    stop() {
+        BdApi.showNotice(randomQuote)
+        console.log("[Positivity] Plugin stopped.");
+        BdApi.alert("💡 Positivity 💡", "Positivity plugin has been shutdown. See you next time, though!");
+    }
+
+    // load() {
+    //    fetch("https://mdev123.github.io/betterdiscord-positivity/positivity.plugin.js", {cache: "no-store"}).then(res => res.text()).then(res => {
+	//		let newVersion = Number(res.substring(res.indexOf("version") + 8, res.indexOf("version") + 13).replaceAll('.', ''))
+	//		if (newVersion > version) {
+	//			console.log("UPDATING!")
+	//           BdApi.alert("Update", "Plugin auto-updating!")
+	//		}
+	//	})
+   // }
+
+  //  constructor(c) {
+  //      this.config = c
+  //  }
+};
